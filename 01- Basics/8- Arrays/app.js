ae85fd1facca5f2affc886c635c28530a4891eb0
Chirag Agrawal <chirag@192.168.1.3>
2025-06-28 22:31:17 +0530

    feat: added splice method

--- 01- Basics/8- Arrays/app.js
+++ 01- Basics/8- Arrays/app.js
@@ -41,6 +41,37 @@ console.log(names);
 names.pop();
 names.pop();
 console.log(names);
-// Removes elements from the array and mutates it
-names.splice(2, 2); // Removes 2 elements starting from position 2 or index 1.
-console.log(names);
+// Splice method in js is used to change content of the array removing, replacing or adding elements at a specified position.
+// array.splice(startIndex, deleteCount, item1, item2, ...., itemN)
+
+// Remove elements from array
+let fruits = ["apple", "banana", "mango", "orange"];
+let removed = fruits.splice(1, 2);
+
+console.log(fruits); // ["apple", "orange"]
+console.log(removed); // ["banana", "mango"]
+
+// Add elements to the array
+const fruits2 = ["apple", "orange"];
+fruits2.splice(1, 0, "banana", "mango");
+
+console.log(fruits2); // ["apple", "banana", "mango", "orange"]
+
+// Replace elements
+let colors = ["red", "blue", "green"];
+colors.splice(1, 1, "yellow");
+
+console.log(colors); // ["red", "yellow", "green"]
+
+// Remove all elements from certain index
+let nums = [1, 2, 3, 4, 5];
+let removedNums = nums.splice(2);
+
+console.log(nums); // [1, 2]
+console.log(removedNums); // [3, 4, 5]
+
+// Using negative index
+let letters = ["a", "b", "c", "d"];
+letters.splice(-2, 1);
+
+console.log(letters); // ["a", "b", "d"]
